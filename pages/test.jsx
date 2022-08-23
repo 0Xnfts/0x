25503a8df9a6f421f6c23e4c518f8ad141072e09
WessJa <123456asred@163.com>
2022-08-23 18:13:15 +0800

    提交代码

--- pages/test.jsx
+++ pages/test.jsx
@@ -65,7 +65,7 @@ const Home = () => {
          }
        }>Let's go! </button>
      </div>
-     <div className='flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center'>
+     <div className='flex gap-y-12 mt-4 w-5/6 gap-x-2 justify-center'>
        {
          NFTs.length && NFTs.map(nft => {
            return (
